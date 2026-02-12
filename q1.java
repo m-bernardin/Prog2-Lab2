8f39171c5e395ddaad844c381d6114794304dd68
m-bernardin <mjrb4mjrb@gmail.com>
2026-02-12 14:12:27 -0500

    basic classes structure

---
+++ q1.java
@@ -0,0 +1,8 @@
+//1-Create class Student (having name and course_code[] attributes), and class Course (having course_code and course_name attributes). 
+// Using array of objects, accept info for some students and courses, and then show the information as his name, and all course names he has taken.
+import java.util.*;
+public class q1{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ q2.java
@@ -0,0 +1,11 @@
+//2-Using an array list of Integer type accept some values from the user (as long as he wants to input),
+//and then insert them, one by one, into the arraylist. Each time the user inputs a number,
+//your code must find out the appropriate location for that value and insert it accordingly. Like this,
+//when the user stops inputting more values, code would show the list of all inputted numbers, sorted in
+//ascending order.
+import java.util.ArrayList;
+public class q2 {
+    public static void main(String[] args) {
+        
+    }
+}
